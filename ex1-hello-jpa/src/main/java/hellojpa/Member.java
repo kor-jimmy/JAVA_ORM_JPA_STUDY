0991d65be68e6014457c7a9009f7fb1bf6930ff6
JinTak Yoo <jintakyoo@JinTakui-MacBookPro.local>
2022-12-30 12:45:00 +0900

    기본키 맵핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,14 +2,35 @@ package hellojpa;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
-public enum Member {
+public class Member {
 
-	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	@Column(name = "name", nullable = false)
 	private String username;
+
+	private Member() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
